15036fec8a7f40b5405c9b9f14ce072ef0bc8416
MinaSameh1 <minasameh1@gmail.com>
2024-01-19 20:58:46 +0200

    refactor(swapi_service): make tests more readable and add two more

--- src/modules/SWApi/__tests__/swapi.service.spec.ts
+++ src/modules/SWApi/__tests__/swapi.service.spec.ts
@@ -1,44 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { HttpService } from 'src/common/services'
+import { AppServicesModule } from 'src/common/services/app.service'
 import { SWApiService } from '../swapi.service'
 import { generateManyFakeSwapiPeople } from './generator'
-import { AppServicesModule } from 'src/common/services/app.service'
 
 describe('SwapiService', () => {
-  let service: SWApiService
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [SWApiService],
-    })
-      .useMocker(token => {
-        if (token === HttpService) {
-          return {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            get: (_url: string) => {
-              const results = generateManyFakeSwapiPeople()
-              return {
-                results,
-                count: results.length,
-                next: null,
-                previous: null,
-              }
-            },
-          }
-        }
-        return null
-      })
-      .compile()
-
-    service = module.get<SWApiService>(SWApiService)
-  })
-
-  // Check all is fine
-  it('should be defined', () => {
-    expect(service).toBeDefined()
-  })
-
   describe('Unit Testing', () => {
+    let service: SWApiService
+
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [SWApiService],
+      })
+        .useMocker(token => {
+          if (token === HttpService) {
+            return {
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              get: (_url: string) => {
+                const results = generateManyFakeSwapiPeople()
+                return {
+                  results,
+                  count: results.length,
+                  next: null,
+                  previous: null,
+                }
+              },
+            }
+          }
+          return null
+        })
+        .compile()
+
+      service = module.get<SWApiService>(SWApiService)
+    })
+
+    // Check all is fine
+    it('should be defined', () => {
+      expect(service).toBeDefined()
+    })
+
     it('should return movies', async () => {
       const movies = await service.getMovies()
       expect(movies).toBeDefined()
@@ -65,7 +65,9 @@ describe('SwapiService', () => {
   })
 
   // Does actual HTTP calls
-  describe.skip('Integration Testing', () => {
+  describe('Integration Testing', () => {
+    let service: SWApiService
+
     beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
         imports: [AppServicesModule.forFeature(HttpService.name)],
@@ -75,10 +77,29 @@ describe('SwapiService', () => {
       service = module.get<SWApiService>(SWApiService)
     })
 
+    // Check all is fine
+    it('should be defined', () => {
+      expect(service).toBeDefined()
+    })
+
     it('should return movies', async () => {
       const movies = await service.getMovies()
       expect(movies).toBeDefined()
       expect(movies.results.length).toBeGreaterThan(0)
+      expect(movies.results[0].title).toBeDefined()
+      expect(typeof movies.results[0].title).toBe('string')
+    })
+
+    it('should make sure the movie shape is correct', async () => {
+      const movies = await service.getMovies()
+      expect(movies).toBeDefined()
+      expect(movies.results.length).toBeGreaterThan(1)
+      expect(movies.results[0].title).toBeDefined()
+      expect(typeof movies.results[0].title).toBe('string')
+      expect(movies.results[0].characters).toBeDefined()
+      expect(Array.isArray(movies.results[0].characters)).toBeTruthy()
+      expect(movies.results[0].characters.length).toBeGreaterThan(0)
+      expect(movies.results[0].episode_id).toBeDefined()
     })
 
     it('should accept a name parameter', async () => {
@@ -92,6 +113,17 @@ describe('SwapiService', () => {
       const people = await service.getPeople()
       expect(people).toBeDefined()
       expect(people.results.length).toBeGreaterThan(0)
+    })
+
+    it('should make sure people shape is correct', async () => {
+      const people = await service.getPeople()
+      expect(people).toBeDefined()
+      expect(people.results.length).toBeGreaterThan(1)
+      expect(typeof people.results[0].name).toBe('string')
+      expect(Array.isArray(people.results[0].films)).toBeTruthy()
+      expect(people.results[0].homeworld).toBeDefined()
+      expect(typeof people.results[0].height).toBe('string')
+      expect(people.results[0].gender).toBeDefined()
     })
 
     it('should accept a name parameter', async () => {
