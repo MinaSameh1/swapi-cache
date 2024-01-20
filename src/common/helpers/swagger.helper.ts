28859888c9ad65e4eaac838d138736ecb2b93490
MinaSameh1 <minasameh1@gmail.com>
2024-01-20 22:20:44 +0200

    misc(swagger): add the patch from nestjs-zod

--- src/common/helpers/swagger.helper.ts
+++ src/common/helpers/swagger.helper.ts
@@ -1,7 +1,9 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { patchNestJsSwagger } from 'nestjs-zod'
 import { ApplicationType } from '../types'
 
 export function setupSwagger(app: ApplicationType) {
+  patchNestJsSwagger()
   const options = new DocumentBuilder()
     .setTitle('Star Wars Api')
     .setDescription('THE Star Wars API, your gateway to the galaxy')
