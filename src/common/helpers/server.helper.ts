cbd805f9e87c33a09959ceca4e492a110f7a7bf7
MinaSameh1 <minasameh1@gmail.com>
2024-01-20 18:37:14 +0200

    chore: add helmet

--- src/common/helpers/server.helper.ts
+++ src/common/helpers/server.helper.ts
@@ -1,3 +1,4 @@
+import helmet from '@fastify/helmet'
 import { NestFactory } from '@nestjs/core'
 import {
   FastifyAdapter,
@@ -6,3 +7,18 @@ import {
 
 export const createNestApp = (module: any) =>
   NestFactory.create<NestFastifyApplication>(module, new FastifyAdapter())
+
+export const setupHelmet = (app: NestFastifyApplication) => {
+  app.register(helmet, {
+    // To solve swagger-ui issues with CSP
+    // See https://docs.nestjs.com/openapi/introduction
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', 'validator.swagger.io'],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+      },
+    },
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
 import { AppModule } from './app.module'
 import { CustomConfigService } from './common/config'
 import { HttpExceptionFilter } from './common/filters'
+import { createNestApp, setupHelmet } from './common/helpers/server.helper'
 import { setupSwagger } from './common/helpers/swagger.helper'
 import { LoggerInterceptor } from './common/interceptors'
-import { createNestApp } from './common/helpers/server.helper'
 
 async function bootstrap() {
   /******************* Create the app *******************/
   const app = await createNestApp(AppModule)
 
-  /******************* App Config *******************/
+  /******************* App Config ****************/
   app.enableVersioning()
   app.enableCors()
 
   /******************* Swagger *******************/
   setupSwagger(app)
+  /******************* Helmet ********************/
+  setupHelmet(app)
 
   /******************* Global Interceptors and Filters *******************/
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalInterceptors(new LoggerInterceptor())
 
-  /******************* Start the app *******************/
+  /******************* Start the app **************/
   const configService = app.get(CustomConfigService)
   await app.listen(configService.getPort())
 }
