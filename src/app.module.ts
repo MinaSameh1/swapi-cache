da73887c50a68efe7ddfeba4de283c2b65aa7f68
MinaSameh1 <minasameh1@gmail.com>
2024-01-22 06:03:58 +0200

    chore: use redis instead of memory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
+import { redisStore } from 'cache-manager-redis-store'
 import type { RedisClientOptions } from 'redis'
 import { ConfigModule, CustomConfigService } from './common/config'
 import { DatabaseModule } from './common/database'
@@ -13,6 +14,7 @@ import { PeopleModule } from './modules/people/people.module'
       isGlobal: true,
       imports: [ConfigModule],
       useFactory: async (configService: CustomConfigService) => ({
+        store: redisStore as any,
         url: configService.getRedisUrl(),
       }),
       inject: [CustomConfigService],
